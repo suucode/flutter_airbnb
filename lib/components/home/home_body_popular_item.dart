23a3e05af988406f928171857cba335edb6953f0
suhyeon <limmm.mary@gmail.com>
2022-11-10 17:11:16 +0900

    완성

--- lib/components/home/home_body_popular_item.dart
+++ lib/components/home/home_body_popular_item.dart
@@ -1,54 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/constants.dart';
 import 'package:flutter_airbnb/size.dart';
+import 'package:flutter_airbnb/styles.dart';
 
 class HomeBodyPopularItem extends StatelessWidget {
-  HomeBodyPopularItem({required this.id, Key? key}) : super(key: key);
-
   final id;
   final popularList = [
     "p1.jpeg",
     "p2.jpeg",
     "p3.jpeg",
   ];
+
+  HomeBodyPopularItem({required this.id});
+
   @override
   Widget build(BuildContext context) {
     //전체 1000이면 popularItemWidth = 700/3-5= 228
     double popularItemWidth = getBodyWidth(context) / 3 - 5;
 
-    return Container(
-      color: Colors.green,
-      height: 200,
-      width: popularItemWidth,
-      constraints: BoxConstraints(
-        minWidth: 320,
-      ),
-      child: SizedBox(
-        width: popularItemWidth,
-        child: Column(
-          children: [
-            _buildPopularItemImage(),
-            _buildPopularItemStar(),
-            _buildPopularItemComment(),
-            _buildPopularItemUserInfo(),
-          ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: gap_xl),
+      child: Container(
+        constraints: BoxConstraints(
+          minWidth: 320,
+        ),
+        child: SizedBox(
+          width: popularItemWidth,
+          child: Column(
+            children: [
+              _buildPopularItemImage(),
+              _buildPopularItemStar(),
+              _buildPopularItemComment(),
+              _buildPopularItemUserInfo(),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _buildPopularItemImage() {
-    return SizedBox();
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset("assets/${popularList[id]}", fit: BoxFit.cover),
+        ),
+        SizedBox(height: gap_s),
+      ],
+    );
   }
 
   Widget _buildPopularItemStar() {
-    return SizedBox();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Icon(Icons.star, color: kAccentColor),
+            Icon(Icons.star, color: kAccentColor),
+            Icon(Icons.star, color: kAccentColor),
+            Icon(Icons.star, color: kAccentColor),
+            Icon(Icons.star, color: kAccentColor),
+          ],
+        ),
+        SizedBox(height: gap_s),
+      ],
+    );
   }
 
   Widget _buildPopularItemComment() {
-    return SizedBox();
+    return Column(
+      children: [
+        Text(
+          "깔끔하고 다 갖춰져있어서 좋았어요:) 위치도 완전 좋아용 다들 여기 살고싶다구ㅋㅋㅋㅋㅋ 화장실도 3개에요!!! 5명에서 씻는 것도 전혀 불편함이 없어 좋았어요^^ 이불도 포근하고 좋습니당ㅎㅎ",
+        ),
+        SizedBox(height: gap_s),
+      ],
+    );
   }
 
   Widget _buildPopularItemUserInfo() {
-    return SizedBox();
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage("assets/p1.jpeg"),
+        ),
+        SizedBox(width: gap_s),
+        Column(
+          children: [
+            Text(
+              "데어",
+              style: subtitle1(),
+            ),
+            Text("한국"),
+          ],
+        )
+      ],
+    );
   }
 }
--- lib/components/home/home_header_form.dart
+++ lib/components/home/home_header_form.dart
@@ -9,23 +9,27 @@ class HomeHeaderForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment(-0.6, 0),
-      child: Container(
-        width: 420,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Form(
-          child: Padding(
-            padding: const EdgeInsets.all(gap_l),
-            child: Column(
-              children: [
-                _buildFormTitle(),
-                _buildFormField(),
-                _buildFormSubmit(),
-              ],
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.only(top: gap_m),
+      child: Align(
+        alignment: screenWidth < 520 ? Alignment(0, 0) : Alignment(-0.6, 0),
+        child: Container(
+          width: 420,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Form(
+            child: Padding(
+              padding: const EdgeInsets.all(gap_l),
+              child: Column(
+                children: [
+                  _buildFormTitle(),
+                  _buildFormField(),
+                  _buildFormSubmit(),
+                ],
+              ),
             ),
           ),
         ),
